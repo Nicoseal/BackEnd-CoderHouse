1d167240286a194e70199beb7161daac5f213ce6
Nicoseal <nicolasventieri@gmail.com>
2022-02-06 21:19:08 -0300

    Consigna 1 terminada

--- Back-Clase-6/Desafio/index.js
+++ Back-Clase-6/Desafio/index.js
@@ -1,25 +1,39 @@
 const express = require("express");
 let app = express();
-const PORT = 8080;
-
+let moment = require("moment");
 let path = require("path");
 let {Server: HttpServer} = require("http");
 let {Server:SocketIO} = require("socket.io");
-
-const Productos = require('./ProductsController.js');
-
-app.set("views", path.join(__dirname,"views"));
-app.set("view engine", "ejs")
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs")
+
+app.use(express.static("./public"));
+
+const Productos = require('./ProductsController.js');
 let productos = new Productos();
 
 app.get("/", (req, res, next) => {
-    let products = productos.getAll();
-    res.render("index", {products});
+    res.render("index");
 })
 
-const server = app.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`));
+let http = new HttpServer(app);
+let io = new SocketIO(http);
+
+io.on("connection", socket => {
+    //console.log("Nuevo cliente conectado:", socket.id);
+    socket.emit('init', productos.getAll());
+
+    socket.on("save_product", data => {
+        productos.add(data);
+        io.sockets.emit("reload_products", productos.getAll())
+        console.log(productos.getAll());
+    })
+})
+
+const server = http.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`));
 server.on("error", error => console.log(error));
