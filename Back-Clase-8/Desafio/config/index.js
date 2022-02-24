7c985eab89e6c9b54e268f80813ba84097731d14
maria sol ventieri <nicolasventieri@gmail.com>
2022-02-24 19:59:32 -0300

    funciones con mariaDB terminadas

--- Back-Clase-8/Desafio/config/index.js
+++ Back-Clase-8/Desafio/config/index.js
@@ -9,7 +9,7 @@ let db = {
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'clase8'
+    database: process.env.DB_NAME || 'test'
 }
 
 
--- Back-Clase-8/Desafio/index.js
+++ Back-Clase-8/Desafio/index.js
@@ -13,10 +13,6 @@ let PORT = config.port;
 let http = new HttpServer(app);
 let io = new SocketIO(http);
 
-const products = require('./ProductsController.js');
-const users = require('./UsersController.js');
-const messages = require('./MessagesController.js');
-
 // Middlewares
 app.use(cors("*"));
 
@@ -26,16 +22,61 @@ app.use(express.urlencoded({extended:true}));
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
+// Functions
+(async () => {
+    try {
+        let exist = await db_knex.schema.hasTable("products");
+        if(!exist) {
+            await db_knex.schema.createTable("products", table => {
+                table.increments("id").primary(),
+                table.string("title"),
+                table.float("price"),
+                table.string("thumbnail")
+            });
+        } else {
+            console.log("This table already exists");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+async function addProduct(data) {
+    try {
+        await db_knex.from("products").insert(data);
+        getProducts().then(result => {
+            io.sockets.emit("reload_products", result);
+        }).catch(error => {
+            console.log(error);
+        });
+    } catch(e) {
+        console.log(e);
+        throw e;
+    }
+};
+
+async function getProducts() {
+    try {
+        let response = await db_knex.from("products");
+        response = JSON.parse(JSON.stringify(response));
+        return response;
+    } catch(e) {
+        console.log(e);
+        throw e;
+    }
+};
+
 // Connections
 app.get("/", (req, res, next) => res.render("index"));
 
 io.on("connection", socket => {
-    socket.emit('init', [products.getAll(), products.getAll()]);
-
-    socket.on("save_product", data => {
-        products.add(data);
-        io.sockets.emit("reload_products", products.getAll())
+    getProducts().then(result => {
+        socket.emit('init', [result, []/*messages.getAll()*/]);
+    }).catch(error => {
+        console.log(error);
     });
+
+    socket.on("save_product", data => addProduct(data));
 
     socket.on("new_message", data => {
         if(users.exist(data.email)) {
