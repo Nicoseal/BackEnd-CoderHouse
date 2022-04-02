9494cfbc00d937ffceb9969bc28ef377b9774555
Nicoseal <nicolasventieri@gmail.com>
2022-04-02 10:35:56 -0300

    small improvements

--- Back-Clase-10/Desafio/routes/index.js
+++ Back-Clase-10/Desafio/routes/index.js
@@ -1,5 +1,5 @@
-let productsController = require("../src/controllers/ProductsController")
-const { faker } = require('@faker-js/faker');
+let productsController = require("../src/controllers/ProductsController");
+let cartsController = require("../src/controllers/CartsController");
 let { Router } = require("express");
 let routerProd = new Router();
 let routerCarr = new Router();
@@ -16,62 +16,9 @@ module.exports = app =>{
     routerProd.delete('/:id', productsController.deleteById);
 
     // Carrito
-    routerCarr.post('/', (req, res) => {
-        res.send({id: carritos.create()});
-    })
-
-    routerCarr.delete('/:id', (req, res) => {
-        let ans = carritos.deleteById(req.params.id);
-        if(ans == -1)
-            res.send({error: 405, description: "Carrito no encontrado"});
-        else
-            res.send(ans);
-    })
-
-    routerCarr.get('/:id/productos', (req, res) => {
-        let ans = carritos.getProds(req.params.id);
-        if(ans == -1)
-            res.send({error: 405, description: "Carrito no encontrado"});
-        else
-            res.send(ans);
-    })
-
-    routerCarr.post('/:id/productos/:id_prod', (req, res) => {
-        let prod = productos.getById(req.params.id_prod);
-        if(prod == -1)
-            res.send({error: 404, description: "Producto no encontrado"});
-        let ans = carritos.addProd(req.params.id, prod);
-        if(ans == -1)
-            res.send({error: 405, description: "Carrito no encontrado"});
-        else
-            res.send(ans);
-    })
-
-    routerCarr.delete('/:id/productos/:id_prod', (req, res) => {
-        let ans = carritos.deleteProd(req.params.id, req.params.id_prod);
-        if(ans == -1)
-            res.send({error: 405, description: "Carrito no encontrado"});
-        else if(ans == -2)
-            res.send({error: 404, description: "Producto no encontrado"});
-        else
-            res.send(ans);
-    })
-
-    app.get('/api/productos-test', (req, res) => {
-        let ans = [];
-        for (let i = 0; i < 5; i++) {
-            let prod = {
-                "name": faker.commerce.product(),
-                "description": faker.lorem.paragraph(),
-                "code": parseInt(faker.finance.amount()),
-                "image": faker.image.image(),
-                "price": parseFloat(faker.commerce.price()),
-                "stock": parseInt(faker.finance.amount()),
-                "id": i+1,
-                "timestamp": parseInt(faker.datatype.number()**1.8)
-            };
-            ans.push(prod);
-        }
-        res.send(ans);
-    })
+    routerCarr.post('/', cartsController.create)
+    routerCarr.delete('/:id', cartsController.deleteById)
+    routerCarr.get('/:id/productos', cartsController.getProds)
+    routerCarr.post('/:id/productos/:id_prod', cartsController.addProd)
+    routerCarr.delete('/:id/productos/:id_prod', cartsController.deleteProd)
 }
--- Back-Clase-10/Desafio/src/controllers/CartsController.js
+++ Back-Clase-10/Desafio/src/controllers/CartsController.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 
-class Carritos {
+let file_name = "./src/DB/carts.txt";
+
+class Cart {
     constructor() {
-        this.file_name = "./src/DB/carts.txt";
         try {
-            fs.readFileSync(this.file_name);
+            fs.readFileSync(file_name);
         } catch {
-            fs.writeFileSync(this.file_name, "");
+            fs.writeFileSync(file_name, "");
         }
     }
 
-    create() {
-        const miArchivo = fs.readFileSync(this.file_name);
+    create(req, res) {
+        const miArchivo = fs.readFileSync(file_name);
         let arr = [];
         let new_cart = {};
         if (miArchivo.length > 0)
@@ -23,66 +24,82 @@ class Carritos {
         new_cart.timestamp = Date.now();
         new_cart.productos = [];
         arr.push(new_cart);
-        fs.writeFileSync(this.file_name, JSON.stringify(arr, null, 2));
-        return new_cart.id;
+        fs.writeFileSync(file_name, JSON.stringify(arr, null, 2));
+        res.send({id: new_cart.id});
     }
 
-    deleteById(id) {
-        const miArchivo = fs.readFileSync(this.file_name);
+    deleteById(req, res) {
+        let id = req.params.id;
+        const miArchivo = fs.readFileSync(file_name);
         if (miArchivo.length < 1)
-            return -1;
+            res.send({error: 405, description: "Cart not found"});
         let arr = JSON.parse(miArchivo);
         for (let x = 0;x < arr.length && arr[x].id <= id; x++)
             if (arr[x].id == id) {
                 let deleted = arr.splice(x, 1);
-                fs.writeFileSync(this.file_name, JSON.stringify(arr, null, 2));
-                return deleted;
+                fs.writeFileSync(file_name, JSON.stringify(arr, null, 2));
+                res.send(deleted);
             }
-        return -1;
+        res.send({error: 405, description: "Cart not found"});
     }
 
-    addProd(id, prod) {
-        const miArchivo = fs.readFileSync(this.file_name);
+    addProd(req, res) {
+        let id = req.params.id_prod;
+        let prod;
+        const prodsArchivo = fs.readFileSync("./src/DB/products.txt");
+        if (prodsArchivo.length < 1)
+            res.send({error: 404, description: "Producto no encontrado"});
+        let arr = JSON.parse(prodsArchivo);
+        for (let x = 0;x < arr.length && arr[x].id <= id; x++)
+            if (arr[x].id == id)
+                prod = arr[x];
+        res.send({error: 404, description: "Producto no encontrado"});
+
+        id = req.params.id;
+        const miArchivo = fs.readFileSync(file_name);
         if (miArchivo.length < 1)
-            return -1;
-        let arr = JSON.parse(miArchivo);
+            res.send({error: 405, description: "Cart not found"});
+        arr = JSON.parse(miArchivo);
         for (let x = 0;x < arr.length && arr[x].id <= id; x++)
             if (arr[x].id == id) {
                 arr[x].productos.push(prod);
-                return arr[x];
+                res.send(arr[x]);
             }
-        fs.writeFileSync(this.file_name, JSON.stringify(arr, null, 2));
-        return -1;
+        fs.writeFileSync(file_name, JSON.stringify(arr, null, 2));
+        res.send({error: 405, description: "Cart not found"});
     }
 
-    getProds(id) {
-        const miArchivo = fs.readFileSync(this.file_name);
+    getProds(req, res) {
+        let id = req.params.id;
+        const miArchivo = fs.readFileSync(file_name);
         if (miArchivo.length < 1)
-            return -1;
+            res.send({error: 405, description: "Cart not found"});
         let arr = JSON.parse(miArchivo);
         for (let x = 0;x < arr.length && arr[x].id <= id; x++)
             if (arr[x].id == id)
-                return arr[x].productos;
-        return -1;
+                res.send(arr[x].productos);
+        res.send({error: 405, description: "Cart not found"});
     }
 
-    deleteProd(id, id_prod) {
-        const miArchivo = fs.readFileSync(this.file_name);
+    deleteProd(req, res) {
+        let id = req.params.id;
+        let id_prod = req.params.id_prod;
+        const miArchivo = fs.readFileSync(file_name);
         if (miArchivo.length < 1)
-            return -1;
+            res.send({error: 405, description: "Cart not found"});
         let arr = JSON.parse(miArchivo);
         for (let x = 0;x < arr.length && arr[x].id <= id; x++)
             if (arr[x].id == id) {
                 for (let y = 0;y < arr[x].productos.length; y++) {
                     let deleted = arr[x].productos.splice(x, 1);
-                    fs.writeFileSync(this.file_name, JSON.stringify(arr, null, 2));
-                    return deleted;
+                    fs.writeFileSync(file_name, JSON.stringify(arr, null, 2));
+                    res.send(deleted);
                 }
-                return -2;
+                res.send({error: 404, description: "Product not found"});
             }
-        fs.writeFileSync(this.file_name, JSON.stringify(arr, null, 2));
-        return -1;
+        fs.writeFileSync(file_name, JSON.stringify(arr, null, 2));
+        res.send({error: 405, description: "Cart not found"});
     }
 }
 
-module.exports = Carritos;
+module.exports = new Cart();
