badf7c74af15efd53a5e45320a367504fc9b7115
Nicoseal <nicolasventieri@gmail.com>
2022-04-08 18:09:07 -0300

    ejercicio 11 terminado

--- Back-Clase-11/Desafio/index.js
+++ Back-Clase-11/Desafio/index.js
@@ -1,16 +1,15 @@
 const { faker } = require('@faker-js/faker');
+let { schema, normalize, denormalize } = require("normalizr")
 const express = require("express");
 const moment = require("moment");
 let path = require("path");
 let cors = require("cors");
+const fs = require('fs');
 
 const {Server: HttpServer} = require("http");
 let {Server:SocketIO} = require("socket.io");
 
 let {config} = require("./config");
-let db_maria = require("./config/mariaDB");
-let db_sqlite = require("./config/sqlite");
-let users = require("./UsersController");
 
 let app = express();
 let PORT = config.port;
@@ -26,95 +25,40 @@ app.use(express.urlencoded({extended:true}));
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
-// Functions MariaDB
-(async () => {
+// Messages functions
+async function save(msg) {
     try {
-        let exist = await db_maria.schema.hasTable("products");
-        if(!exist) {
-            await db_maria.schema.createTable("products", table => {
-                table.increments("id").primary(),
-                table.string("title"),
-                table.float("price"),
-                table.string("thumbnail")
-            });
-        } else {
-            console.log("This table already exists, MariaDB");
+        const myFile = await fs.promises.readFile("./DB/messages.txt");
+        let arr = [];
+        msg.id = 1;
+        if (myFile.length > 0)
+        {
+            arr = JSON.parse(myFile);
+            msg.id = arr[arr.length-1].id + 1;
         }
-    } catch (error) {
+        arr.push(msg);
+        await fs.promises.writeFile("./DB/messages.txt", JSON.stringify(arr, null, 2))
+        console.log('Escritura correcta');
+    }
+    catch(error) {
         console.log(error);
     }
-})();
-
-async function addProduct(data) {
-    try {
-        await db_maria.from("products").insert(data);
-        getProducts().then(result => {
-            io.sockets.emit("reload_products", result);
-        }).catch(error => {
-            console.log(error);
-        });
-    } catch(e) {
-        console.log(e);
-        throw e;
-    }
-};
-
-async function getProducts() {
-    try {
-        let response = await db_maria.from("products");
-        response = JSON.parse(JSON.stringify(response));
-        return response;
-    } catch(e) {
-        console.log(e);
-        throw e;
-    }
-};
-
-// Functions Sqlite3
-(async () => {
-    try {
-        let exist = await db_sqlite.schema.hasTable("messages");
-        if(!exist) {
-            await db_sqlite.schema.createTable("messages", table => {
-                table.increments("id").primary(),
-                table.string("email"),
-                table.string("message"),
-                table.string("date")
-            });
-        } else {
-            console.log("This table already exists, Sqlite");
-        }
-    } catch (error) {
-        console.log(error);
-    }
-})();
-
-async function addMessage(data) {
-    try {
-        await db_sqlite.from("messages").insert(data);
-        getMessages().then(result => {
-            io.sockets.emit("reload_messages", result);
-        }).catch(error => {
-            console.log(error);
-        });
-    } catch(e) {
-        console.log(e);
-        throw e;
-    }
-};
+}
 
 async function getMessages() {
     try {
-        let response = await db_sqlite.from("messages");
-        response = JSON.parse(JSON.stringify(response));
-        return response;
+        const myFile = await fs.promises.readFile("./DB/messages.txt");
+        if (myFile.length <= 0) {
+            return null;
+        }
+        return JSON.parse(myFile);
     } catch(e) {
-        console.log(e);ñ
+        console.log(e);
         throw e;
     }
-};
+}
 
-// Test Functions
+// Faker Functions
 function getFakeProds() {
     let ans = [];
     for (let i = 0; i < 5; i++) {
@@ -129,33 +73,36 @@ function getFakeProds() {
     return ans;
 };
 
+// Normalizer
+const user = new schema.Entity('authors');
+const messageSchema = new schema.Entity('message', {
+    author: user
+});
+const messagesSchema = new schema.Entity('messages', {
+    messages: [messageSchema]
+});
+
 // Connections
 app.get("/", (req, res) => res.render("index"));
-app.get("/test", (req, res) => res.render("faker", {faked: getFakeProds()}));
 
 io.on("connection", socket => {
-    getProducts().then(result_prods => {
+    getMessages().then(result_msg => {
+        let normalized = normalize({id: 'messages', messages: result_msg}, messagesSchema);
+        let compression_value = 100 - parseInt(JSON.stringify(normalized).length * 100 / JSON.stringify(result_msg).length);
+        socket.emit('init', [getFakeProds(), {value: normalized, compression: compression_value}]);
+    }).catch(err => {
+        console.log(err);
+    });
+
+    socket.on("new_message", data => {
+        save({...data, date: moment().format("DD/MM/YYYY HH:MM:SS")});
         getMessages().then(result_msg => {
-            socket.emit('init', [result_prods, result_msg]);
+            let normalized = normalize({id: 'messages', messages: result_msg}, messagesSchema);
+            let compression_value = 100 - parseInt(JSON.stringify(normalized).length * 100 / JSON.stringify(result_msg).length);
+            socket.emit('reload_messages', {value: normalized, compression: compression_value});
         }).catch(err => {
             console.log(err);
         });
-    }).catch(error => {
-        console.log(error);
-    });
-
-    socket.on("save_product", data => addProduct(data));
-
-    socket.on("new_message", data => {
-        if(users.exist(data.email)) {
-            if(users.isValid(data.email, socket.id))
-                addMessage({...data, date: moment().format("DD/MM/YYYY HH:MM:SS")});
-            else
-                socket.emit("error", `El usuario ${data.email} ya está en uso`);
-        } else {
-            users.create({email:data.email, id:socket.id, status:"active"});
-            addMessage({...data, date: moment().format("DD/MM/YYYY HH:MM:SS")});
-        }      
     });
 });
 
